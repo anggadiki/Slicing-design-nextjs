b072be06266514e56e0d7306daf0d36d0631499e
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2023-10-31 12:34:33 +0700

    Membuat folder awal

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return <div className="">Hello about</div>;
+};
+
+export default AboutPage;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div className=" flex flex-col min-h-screen items-center justify-center ">
+      Dashboard Slcing
+    </div>
+  );
+};
+
+export default DashboardPage;
