0bea2d1066496074c27f6864e16a15a13b249c4d
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2023-11-01 18:37:21 +0700

    membuat navbar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,25 @@
 const DashboardPage = () => {
   return (
-    <div className=" flex flex-col min-h-screen items-center justify-center ">
-      Dashboard Slicing
+    <div className="container min-w-full">
+      <div className=" flex bg-[#F68533] p-2 pt-4">
+        <div className=" w-3/12">
+          <img src="/logo.png" alt="" width="200" height="200" />
+        </div>
+        <div className=" w-6/12">
+          <ul className=" flex justify-center gap-4 font">
+            <li>Home</li>
+            <li>Chilcare Juice</li>
+            <li>Order</li>
+            <li>Blog</li>
+            <li>FAQ</li>
+          </ul>
+        </div>
+        <div className=" w-3/12 flex justify-end">
+          <img src="/account.svg" alt=" account" width="55" height="55" />
+          <img src="/cart-plus.svg" alt=" account" width="55" height="55" />
+          <img src="/magnify.svg" alt=" account" width="55" height="55" />
+        </div>
+      </div>
     </div>
   );
 };
