2f5d3abaacf224e48e15d19ad047b39e991ef887
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2023-11-05 21:26:20 +0700

    fix margin and padding for footer

--- src/components/fragments/Footer.tsx
+++ src/components/fragments/Footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="">
       <div className=" flex px-20">
-        <div className=" flex flex-col w-5/12 px-5">
-          <div className=" py-6 font-semibold text-6xl text-[#F68533]">
+        <div className=" flex flex-col w-5/12 px-5 -mt-12">
+          <div className=" py-1 font-semibold text-6xl text-[#F68533]">
             <p className="text-juice">About</p>
           </div>
           <div className=" flex justify-start">
